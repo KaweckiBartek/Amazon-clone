126556beb99b64a10ed7a5892eea60675f077e8c
KaweckiBartek <67326141+KaweckiBartek@users.noreply.github.com>
2021-01-03 17:59:55 +0100

    setup amazon react clone

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2uRAC5zl3rHY-dwz6ywLLuHd4IIW5LQ8",
+  authDomain: "clone-1178a.firebaseapp.com",
+  projectId: "clone-1178a",
+  storageBucket: "clone-1178a.appspot.com",
+  messagingSenderId: "794409819764",
+  appId: "1:794409819764:web:b18b52e444ded2bbd97b21",
+  measurementId: "G-MHC3T5QPEE"
+};
